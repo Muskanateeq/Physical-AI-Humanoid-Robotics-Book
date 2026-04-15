049903c63902dd266175377ce428cf8f0d83a200
Muskanateeq <ateeq.in.com@gmail.com>
2026-04-15 16:11:49 +0500

    set chatkit production url

--- physical-ai-humanoid-robotics/docusaurus.config.js
+++ physical-ai-humanoid-robotics/docusaurus.config.js
@@ -20,7 +20,7 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: "https://neurobotics-book.vercel.app",
+  url: "https://neurobotics-ai-book.vercel.app",
   // Set the /<baseUrl>/ pathname under which your site is served
   baseUrl: "/",
   trailingSlash: true,
